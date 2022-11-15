097f3c00be56f828834eae012be0e034fc13c196
Mika2147 <mika.auer@icloud.com>
2022-11-15 11:20:17 +0100

    Changed from java to python

---
+++ chatbot_sematic_technologies/app.py
@@ -0,0 +1,17 @@
+from rivescript import RiveScript
+
+bot = RiveScript()
+bot.load_directory("./Rivescripts")
+bot.sort_replies()
+
+run = True
+
+while run is True:
+    print("User:")
+    user_input = input()
+
+    if user_input == "quit":
+        run = False
+    else:
+        reply = bot.reply("user", user_input)
+        print("Eliza: \n" + reply)
